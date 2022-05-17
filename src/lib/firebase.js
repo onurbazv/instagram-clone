c214c949a84e64fa4059027092c748edbc4bf176
Bruno Azevedo <onurbazv@gmail.com>
2022-05-17 15:32:13 -0300

    firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,13 +1,6 @@
-const config = {
-    apiKey: "AIzaSyC3kiZ04bfJsZKmu0BETSseZwnuRrowYls",
-    authDomain: "instagram-clone-c0b0b.firebaseapp.com",
-    projectId: "instagram-clone-c0b0b",
-    storageBucket: "instagram-clone-c0b0b.appspot.com",
-    messagingSenderId: "736943222937",
-    appId: "1:736943222937:web:324739fb684c59dfc74808"
-  };
+import firebaseConfig from '../constants/firebaseConfig'
 
-const firebase = window.firebase.initializeApp(config)
+const firebase = window.firebase.initializeApp(firebaseConfig)
 const storageRef = firebase.storage().ref()
 const { FieldValue } = window.firebase.firestore
 
